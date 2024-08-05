da324bcbe0b7d4d01c324214a1641316f6929641
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-05 20:08:49 +0300

    Add controllers/UserControllers.js and a new route to add new users to the database

---
+++ controllers/UserController.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UserController {
+  static async postNew(req, res) {
+    if (dbClient.isAlive()) {
+      const { email, password } = req.body;
+      if (!email) { return res.status(400).json({ error: 'Missing email ' }); }
+      if (!password) { return res.status(400).json({ error: 'Missing password' }); }
+
+      const userCollection = await dbClient.userCollection;
+      const emailAdd = await userCollection.findOne({ email });
+      if (emailAdd) { return res.status(400).json({ error: 'Already exists' }); }
+
+      const passwordHash = sha1(password);
+
+      const newUser = await userCollection.insertOne({ email, passwordHash });
+      return res.status(201).json({ email, id: newUser.insertedId });
+    }
+    return null;
+  }
+}
+module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UserController';
 
 const indexRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+  app.post('/users', UserController.postNew);
 };
 
 export default indexRoutes;
