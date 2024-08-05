c7467a5ac89923c2a958157a5d887228ddcc2ff4
Dridi Chaith <dridi.chaith@gmail.com>
2024-08-05 22:23:09 +0100

    task 3 added back

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.use('/status', AppController.getStatus);
 router.use('/stats', AppController.getStats);
+router.use('/users', UsersController.postNew);
 
 module.exports = router;
