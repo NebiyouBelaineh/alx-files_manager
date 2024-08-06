a9a5d368a2b13bffddedd6d0fe6d325597341deb
Dridi Chaith <dridi.chaith@gmail.com>
2024-08-06 01:47:37 +0100

    task 2 taking off the test for the db alive

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,21 +3,18 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    if (dbClient.isAlive()) {
-      const { email, password } = req.body;
-      if (!email) { return res.status(400).json({ error: 'Missing email' }); }
-      if (!password) { return res.status(400).json({ error: 'Missing password' }); }
+    const { email, password } = req.body;
+    if (!email) { return res.status(400).json({ error: 'Missing email' }); }
+    if (!password) { return res.status(400).json({ error: 'Missing password' }); }
 
-      const userCollection = await dbClient.userCollection;
-      const emailAdd = await userCollection.findOne({ email });
-      if (emailAdd) { return res.status(400).json({ error: 'Already exists' }); }
+    const userCollection = await dbClient.userCollection;
+    const emailAdd = await userCollection.findOne({ email });
+    if (emailAdd) { return res.status(400).json({ error: 'Already exists' }); }
 
-      const passwordHash = sha1(password);
+    const passwordHash = sha1(password);
 
-      const newUser = await userCollection.insertOne({ email, passwordHash });
-      return res.status(201).json({ _id: newUser.insertedId, email });
-    }
-    return null;
+    const newUser = await userCollection.insertOne({ email, passwordHash });
+    return res.status(201).json({ _id: newUser.insertedId, email });
   }
 }
 
