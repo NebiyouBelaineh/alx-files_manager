eca2059e53a5e7f8fbfdd209b42efb7dcafce2fa
Dridi Chaith <dridi.chaith@gmail.com>
2024-08-06 01:52:27 +0100

    task 2 add parse url-encoded bodies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const port = process.env.PORT || 5000;
 const app = express();
-
+app.use(express.urlencoded());
 app.use(express.json());
 
 app.use('/', require('./routes/index'));
