dc6788fd4093f84dbe5aec14ba3864f521baa464
Dridi Chaith <dridi.chaith@gmail.com>
2024-08-06 03:07:48 +0100

    task 3 taking off the console.log

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,6 @@ import dbClient from '../utils/db';
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    console.log(email, password);
     if (!email) { return res.status(400).json({ error: 'Missing email' }); }
     if (!password) { return res.status(400).json({ error: 'Missing password' }); }
 
